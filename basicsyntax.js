dd70391a854ee36a20d97e9f9f02c038d0414048
Ethan Liang <ethanliang97@gmail.com>
2015-11-05 15:12:08 -0500

    Ventured into ng-repeat

--- basicsyntax.js
+++ basicsyntax.js
@@ -11,11 +11,16 @@ $scope.favoriteColor;
     
 }]);
 
-angular.module('css', []).controller("index", ["$scope", function ($scope) {
-    $scope.value =1; 
-    
-    $scope.isBold = function() { return $scope.value %2 === 0;}; 
-    $scope.isItalic = function () { return $scope.value%3 ===0; };
-    $scope.isUnderline = function() { return $scope.value %5 === 0;}; 
+angular.module("css", [])
+	.controller("index", ["$scope", function ($scope) {
+		$scope.value;
+		$scope.Bold = function () { return $scope.value % 2 === 0; };
+		$scope.Italic = function () { return $scope.value % 3 === 0; };
+		$scope.Underlined = function () { return $scope.value % 5 === 0; };
+	}]);
 
-}]);
+angular.module("repeater", [])
+    .controller("index", ["$scope", function ($scope){
+        
+        
+    }])
